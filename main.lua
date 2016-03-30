eb8ce5e0761d82089735ef691f3aa863d887dc8b
alanquadros <alanquadros@hotmail.com>
2016-03-30 16:33:25 -0300

    Atualizações

--- main.lua
+++ main.lua
@@ -1,10 +1,10 @@
+HC = require 'HC'
+
 cooldown = 20
 enemy = {}
 enemies_controller = {}
-enemies_controller.enemies = {}
-left_enemy_x = 30
-right_enemy_x = 800
-enemy_side = false --false equal left
+enemies_controller.enemies_left = {}
+enemies_controller.enemies_right = {}
 
 function love.load()
 	love.keyboard.setKeyRepeat(false)
@@ -12,6 +12,7 @@ function love.load()
 	player.left = false
 	player.right = false
 	player.left_x = 300
+	player.right_x = 450
 	player.cooldown = cooldown
 	player.keydown = function()
 		if player.cooldown <= 0 then
@@ -19,30 +20,52 @@ function love.load()
 			player.right = false
 		end
 	end
-	for i = 0, 10 do
-		enemies_controller:spawnEnemy(30)		
-		--enemies_controller:spawnEnemy(800)
-	end
+	--for i = 0, 10 do
+	enemies_controller:spawnEnemyLeft()
+	enemies_controller:spawnEnemyRight()
+	--end
+
+	
 end
 
-function checkCollisions(enemies)
-	for i, e in ipairs(enemies) do
+function checkCollisionsLeft(enemies_left)
+	for i, e in ipairs(enemies_left) do
 		if player.left then
-			if player.left_x <= e.y + e.height and player.left_x > e.x and player.left_x < e.x + e.width then
-				table.remove(enemies, i)
+			if player.left_x > e.x and player.left_x < e.x + e.width then
+				table.remove(enemies_left, i)
 			end
 		end			
 	end
 end
 
-function enemies_controller:spawnEnemy(x)
+function checkCollisionsRight(enemies_right)
+	for i, e in ipairs(enemies_right) do
+		if player.right then
+			if player.right_x > e.x and player.right_x < e.x + e.width then
+				table.remove(enemies_right, i)
+			end
+		end			
+	end
+end
+
+function enemies_controller:spawnEnemyLeft()
 	enemy = {}
-	enemy.x = x
+	enemy.x = 30
 	enemy.y = 450
 	enemy.width = 50
   	enemy.height = 100
   	enemy.speed = 2
-	table.insert(self.enemies, enemy)
+	table.insert(self.enemies_left, enemy)
+end
+
+function enemies_controller:spawnEnemyRight()
+	enemy = {}
+	enemy.x = 800
+	enemy.y = 450
+	enemy.width = 50
+  	enemy.height = 100
+  	enemy.speed = 2
+	table.insert(self.enemies_right, enemy)
 end
 
 function love.keyreleased(key)
@@ -72,35 +95,41 @@ function love.update(dt)
 		player.keydown()
 	end
 
-	for _,e in pairs(enemies_controller.enemies) do
+	for _,e in pairs(enemies_controller.enemies_left) do
 	    if e.x >= love.graphics.getHeight() then
-	    	print('fim')
+	    	--print('fim')
 	    end
-
-	    if not enemy_side then
-	    	e.x = e.x + 1 * e.speed
-	    else
-	    	e.x = e.x - 1 * e.speed
-	    end
-	    
+		e.x = e.x + 1 * e.speed    
   	end
-  	checkCollisions(enemies_controller.enemies, player.left)
+
+  	for _,e in pairs(enemies_controller.enemies_right) do
+	    if e.x >= love.graphics.getHeight() then
+	    	--print('fim')
+	    end
+	    e.x = e.x - 1 * e.speed	    
+  	end
+
+  	checkCollisionsLeft(enemies_controller.enemies_left, player.left)
+  	checkCollisionsRight(enemies_controller.enemies_right, player.rigth)
 end
 
 function love.draw()
 	if player.left then
 		player.right = false
-		love.graphics.rectangle("fill", player.left_x, 400, 50, 100, 10, 10)		
+		love.graphics.rectangle("fill", player.left_x, 400, 10, 100, 10, 10)		
 	end
 	
 	if player.right then
 		player.left = false
-		love.graphics.rectangle("fill", 450, 400, 50, 100, 10, 10)
+		love.graphics.rectangle("fill", player.right_x, 400, 10, 100, 10, 10)
 	end
 
-	for _, e in pairs(enemies_controller.enemies) do
+	for _, e in pairs(enemies_controller.enemies_left) do
 		love.graphics.circle("fill", e.x, e.y, e.width, e.height)
 	end
 	
+	for _, e in pairs(enemies_controller.enemies_right) do
+		love.graphics.circle("fill", e.x, e.y, e.width, e.height)
+	end
 
 end
